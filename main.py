66335a5aa573ccf19cf7f1ce51c2e6ac22a9e5ed
pratik <pratiksharmavip@gmail.com>
2022-07-02 11:15:21 +0530

    turtle moveing up/down

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
+# screen
+
+from tkinter import N
 from turtle import *
+import turtle
 screen = Screen()
+screen.title("Pong")
 screen.bgcolor("black")
 screen.setup(800, 600)
+
+# paddel_1
+paddel_1=Turtle()
+paddel_1.speed("fastest")
+paddel_1.hideturtle()
+paddel_1.color('white')
+paddel_1.shape("square")
+paddel_1.shapesize(5,1)
+paddel_1.penup()
+paddel_1.goto(380,0)
+paddel_1.showturtle()
+
+def go_up():
+    newx=(paddel_1.ycor()+20)
+    paddel_1.goto(paddel_1.xcor(),newx)
+def go_dn():
+    newx=(paddel_1.ycor()-20)
+    paddel_1.goto(paddel_1.xcor(),newx)
+turtle.listen()
+onkeypress(go_up,"Up")
+turtle.listen()
+onkeypress(go_dn,"Down")
 screen.exitonclick()
